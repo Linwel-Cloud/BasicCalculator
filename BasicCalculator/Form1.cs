1d9917d407fbec60421d1a20f4ddf5ec0a07d280
User <User@LAPTOP-1LRDH7J7>
2025-09-10 21:41:10 +0800

    Add project files.

---
+++ BasicCalculator/Form1.cs
@@ -0,0 +1,84 @@
+namespace BasicCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int num1;
+        int num2;
+        string oprt;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            num1 = 0;
+            num2 = 0;
+            oprt = "";
+        }
+    }
+}
